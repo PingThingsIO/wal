856a42c0f754001e96178a815d096fc2a25fb504
HuangYi <huang@crypto.com>
2023-06-20 12:49:17 +0800

    unit test

--- wal_test.go
+++ wal_test.go
@@ -618,6 +618,8 @@ func TestOutliers(t *testing.T) {
 	t.Run("fail-corrupted-tail-json", func(t *testing.T) {
 		defer os.RemoveAll("testlog/corrupt-tail")
 		opts := makeOpts(512, true, JSON, false)
+		optsRecoverTail := *opts
+		optsRecoverTail.RecoverCorruptedTail = true
 		os.MkdirAll("testlog/corrupt-tail", 0777)
 		ioutil.WriteFile(
 			"testlog/corrupt-tail/00000000000000000001",
@@ -626,12 +628,20 @@ func TestOutliers(t *testing.T) {
 			l.Close()
 			t.Fatalf("expected %v, got %v", ErrCorrupt, err)
 		}
+		if l, err := Open("testlog/corrupt-tail", &optsRecoverTail); err != nil {
+			l.Close()
+			t.Fatalf("expected %v, got %v", nil, err)
+		}
 		ioutil.WriteFile(
 			"testlog/corrupt-tail/00000000000000000001",
 			[]byte(`{}`+"\n"), 0666)
 		if l, err := Open("testlog/corrupt-tail", opts); err != ErrCorrupt {
 			l.Close()
 			t.Fatalf("expected %v, got %v", ErrCorrupt, err)
+		}
+		if l, err := Open("testlog/corrupt-tail", &optsRecoverTail); err != nil {
+			l.Close()
+			t.Fatalf("expected %v, got %v", nil, err)
 		}
 		ioutil.WriteFile(
 			"testlog/corrupt-tail/00000000000000000001",
@@ -640,12 +650,20 @@ func TestOutliers(t *testing.T) {
 			l.Close()
 			t.Fatalf("expected %v, got %v", ErrCorrupt, err)
 		}
+		if l, err := Open("testlog/corrupt-tail", &optsRecoverTail); err != nil {
+			l.Close()
+			t.Fatalf("expected %v, got %v", nil, err)
+		}
 		ioutil.WriteFile(
 			"testlog/corrupt-tail/00000000000000000001",
 			[]byte(`{"index":"1","data":"?"}`), 0666)
 		if l, err := Open("testlog/corrupt-tail", opts); err != ErrCorrupt {
 			l.Close()
 			t.Fatalf("expected %v, got %v", ErrCorrupt, err)
+		}
+		if l, err := Open("testlog/corrupt-tail", &optsRecoverTail); err != nil {
+			l.Close()
+			t.Fatalf("expected %v, got %v", nil, err)
 		}
 	})
 
